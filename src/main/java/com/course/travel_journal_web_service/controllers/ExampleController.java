a161e67501a055c447a50880b40b14f955fe9098
hgjtu <vlrtmsq@gmail.com>
2025-03-17 01:25:32 +0300

    попытки связать с фронтом

--- src/main/java/com/course/travel_journal_web_service/controllers/ExampleController.java
+++ src/main/java/com/course/travel_journal_web_service/controllers/ExampleController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/example")
+@RequestMapping("/api/v1/example")
 @RequiredArgsConstructor
 @Tag(name = "Аутентификация")
 public class ExampleController {
--- src/main/java/com/course/travel_journal_web_service/controllers/TestController.java
+++ src/main/java/com/course/travel_journal_web_service/controllers/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1")
 @AllArgsConstructor
 public class TestController {
     @GetMapping(path = "/welcome")
