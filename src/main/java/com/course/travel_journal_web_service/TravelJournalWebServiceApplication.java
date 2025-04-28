0194ff1204201a728dd592ea6321fdfe6efd9a39
hgjtu <vlrtmsq@gmail.com>
2025-04-28 23:59:29 +0300

    actuator

--- src/main/java/com/course/travel_journal_web_service/TravelJournalWebServiceApplication.java
+++ src/main/java/com/course/travel_journal_web_service/TravelJournalWebServiceApplication.java
@@ -2,7 +2,9 @@ package com.course.travel_journal_web_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class TravelJournalWebServiceApplication {
 
